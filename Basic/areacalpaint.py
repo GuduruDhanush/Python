0b1d26cf661a996de3eb3b51169f98de6ac70151
GUDURU DHANUSH <69163141+GuduruDhanush@users.noreply.github.com>
2021-05-16 11:28:12 +0530

    Delete Basic directory

--- Basic/areacalpaint.py
+++
@@ -1,6 +0,0 @@
-def area_cal(h, w, coverage=5):
-    no_of_cans = (h*w)/coverage
-    return round(no_of_cans)
-
-
-area_cal(2, 4)
--- Basic/evenorodd.py
+++
@@ -1,6 +0,0 @@
-num = int(input('Enter a number: '))
-
-if num % 2 == 0:
-    print('Entered number is a EVEN number')
-else:
-    print('Its a ODD number')
